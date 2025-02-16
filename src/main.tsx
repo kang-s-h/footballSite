675fc4e12d7509a69c7f603dfb8ed39b8cee6e74
강성훈 <kshevl123@gmail.com>
2025-02-16 20:33:25 +0900

    BrowserRouter basename 수정

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/footballSite">
       <StrictMode>
         <App />
       </StrictMode>
